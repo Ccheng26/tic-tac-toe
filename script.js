708bb8c59418512104cc911d1628c4da1d97b484
Connie Cheng <connie.cheng52@gmail.com>
2017-04-23 23:45:31 -0400

    fix empty row issue

--- script.js
+++ script.js
@@ -24,18 +24,19 @@ $('td').click(
 function winCheck(player){
   if(
     // horizontal
-    $('#1').text() == $('#2').text() && $('#2').text() == $('#3').text() ||
-    $('#4').text() == $('#5').text() && $('#5').text() == $('#6').text() ||
-    $('#1').text() == $('#2').text() && $('#2').text() == $('#3').text() ||
+    $('#1').text() == $('#2').text() && $('#2').text() == $('#3').text() && $('#3').text().length != 0||
+    $('#4').text() == $('#5').text() && $('#5').text() == $('#6').text() && $('#6').text().length != 0||
+    $('#7').text() == $('#8').text() && $('#8').text() == $('#9').text() && $('#9').text().length != 0||
     //vertical
-    $('#1').text() == $('#4').text() && $('#4').text() == $('#7').text() ||
-    $('#2').text() == $('#5').text() && $('#5').text() == $('#8').text() ||
-    $('#3').text() == $('#6').text() && $('#6').text() == $('#9').text() ||
+    $('#1').text() == $('#4').text() && $('#4').text() == $('#7').text() && $('#7').text().length != 0||
+    $('#2').text() == $('#5').text() && $('#5').text() == $('#8').text() && $('#8').text().length != 0||
+    $('#3').text() == $('#6').text() && $('#6').text() == $('#9').text() && $('#9').text().length != 0||
     //diagonal
-    $('#1').text() == $('#5').text() && $('#5').text() == $('#9').text() ||
-    $('#3').text() == $('#5').text() && $('#5').text() == $('#7').text()
+    $('#1').text() == $('#5').text() && $('#5').text() == $('#9').text() && $('#3').text().length != 0||
+    $('#3').text() == $('#5').text() && $('#5').text() == $('#7').text() && $('#3').text().length != 0
     ){
     alert(player + ' win')
+  console.log($('#3').text().length)
   } else{
     if(turn ==8){
       alert('draw')
